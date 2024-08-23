9a191dd0e1e97a2e91b00da318af2601835155f5
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2024-08-23 21:45:15 +0530

    Create Find the Runner-Up Score!.py

---
+++ Basic Data Types/Find the Runner-Up Score!.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    for i in range(n):
+        if arr[i]>arr[i+1]:
+            print(arr[i+1])
+            break
+
+
+
+https://youtu.be/sYTOROnYLCs?si=hevWT_aGmzYPJS1V
