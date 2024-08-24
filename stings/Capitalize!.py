bf00994fff6df3cea17ba57539dbb8b0ed026d29
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2024-08-24 23:51:00 +0530

    Create Capitalize!.py

---
+++ stings/Capitalize!.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    for name in s.split():
+        s=s.replace(name,name.capitalize())
+    return s
+
+if __name__ == '__main__':
+
+
+
+
+
+
+
+
+  https://youtu.be/jp-zFWjIsxU?si=QlOPK0ftcs33IDRR
