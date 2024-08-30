638b0f75fa054419d3f2e4d466c7e9e8ec39830d
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2024-08-30 20:36:50 +0530

    Create Set .intersection() Operation.py

---
+++ sets/Set .intersection() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+a=set(map(int, input().split()))
+m=int(input())
+b=set(map(int, input().split()))
+c=a& set(b)
+print(len(c))
